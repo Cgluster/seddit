5102170960431517c70659ef83a9bc9c4a339e18
unknown <luster.clifford@yahoo.com>
2022-09-03 11:47:01 -0700

    updates

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -1,13 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import './Header.css';
 import { useDispatch, useSelector } from 'react-redux';
-// import { HiOutlineSearch } from 'react-icons/hi';
-// import { FaReddit } from 'react-icons/fa';
-// import { setSearchTerm } from ...
+import { HiOutlineSearch } from 'react-icons/hi';
+import { FaReddit } from 'react-icons/fa';
+import { setSearchTerm } from '../../store/redditSlice';
 
 const Header = () => {
+    const [searchTermLocal, setSearchTermLocal] = useState('');
+    const searchTerm = useSelector((state) => state.reddit.searchTerm);
+    const dispatch = useDispatch();
+
+    const onSearchTermChange = (e) => {
+        setSearchTermLocal(e.target.value);
+    };
+
+    useEffect(() => {
+        setSearchTermLocal(searchTerm);
+    }, [searchTerm]);
+
+    const onSearchTermSubmit = (e) => {
+        e.preventDefault();
+        dispatch(setSearchTerm(searchTermLocal));
+    };
+
     return (
-        <header></header>
+        <header>
+            <div className='logo'>
+                <FaReddit className='logo-icon' />
+                <p>
+                    Seddit
+                </p>
+            </div>
+            <form className='search' onSubmit={onSearchTermSubmit}>
+                <input 
+                type='text'
+                placeholder='search'
+                value={searchTermLocal}
+                onChange={onSearchTermChange}
+                aria-label='Search posts'
+                />
+                <button type='submit' onClick={onSearchTermSubmit} aria-label='Search'>
+                    <HiOutlineSearch />
+                </button>
+            </form>
+        </header>
     );
 }
 
